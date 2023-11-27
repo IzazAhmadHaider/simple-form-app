452380391d8643054f084f218636cffabf022055
izaz ahmad <izazahmadhaider0@gmail.com>
2023-11-27 10:29:15 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data submitted:', formData);
+    // Add your form submission logic here
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Simple Form</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <br />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <br />
+
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+test('renders form and handles submission', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByLabelText(/Name/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
+  expect(screen.getByText(/Submit/i)).toBeInTheDocument();
+
+  
+
+  fireEvent.change(screen.getByLabelText(/Name/i), { target: { value: 'izaz Ahmad' } });
+  fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'izaz@ahmad.com' } });
+
+
+  expect(screen.getByLabelText(/Name/i).value).toBe('izaz Ahmad');
+  expect(screen.getByLabelText(/Email/i).value).toBe('izaz@ahmad.com');
+
+
+  fireEvent.submit(screen.getByText(/Submit/i));
 });
